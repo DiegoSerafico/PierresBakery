f0a2d154cfdfa8ad2402020e488a5ceeaf643f4a
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-24 14:01:58 -0700

    add functionality for calculation customer order

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -16,18 +16,26 @@ namespace PierresBakery
         "\n" +
         "Current Discounts:\n" + 
         "Buy two get one free on bread.\n" + 
-        "Buy 2 get one half off on pastries.\n" +
+        "Buy 2 get one half off on pastries.\n"
       );
       Console.WriteLine("Would you like to make an order?");
-      string willOrder = Console.Readline();
+      string willOrder = Console.ReadLine();
       if (willOrder.ToLower() == "yes")
       {
         Bread bread = new Bread();
         Pastry pastry = new Pastry();
-        Console.WriteLine("How Many Loaves of bread would you like?");
-        int breadAmount = Console.Readline();
+        Console.WriteLine("How many loaves of bread would you like?");
+        int breadAmount = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("How many pastries would you like?");
-        int pastryAmount = Console.Readline();
+        int pastryAmount = Convert.ToInt32(Console.ReadLine());
+        float breadTotal = bread.Order(breadAmount);
+        float pastryTotal = pastry.Order(pastryAmount);
+        float orderTotal = breadTotal + pastryTotal;
+        Console.WriteLine(
+          "Bread Total: $" + breadTotal + "\n" + 
+          "Pastry Total: $" + pastryTotal + "\n" + 
+          "Order Total: $" + orderTotal 
+          );
       }
     }
   }
